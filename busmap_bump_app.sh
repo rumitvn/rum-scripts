ee1746fa6d1a98d8d5a77f128ef57f74f2763d2a
Rum Nguyen <rum123ken@gmail.com>
2024-05-21 18:59:39 +0700

    Update bump app from config too

--- busmap_bump_app.sh
+++ busmap_bump_app.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-# Define project directories
-project_directories=(
-    "/Users/rumnguyen/StudioProjects/busmap-android-2/"
-    "/Users/rumnguyen/StudioProjects/busmap-android/"
-    "/Users/rumnguyen/StudioProjects/vinbus-app-android-temp/"
-)
+CONFIG_DIR="$HOME/.config/rum-scripts"
+CONFIG_FILE="$CONFIG_DIR/config.json"
+
+# Check if .config directory and config.json file exist
+if [ ! -d "$CONFIG_DIR" ]; then
+    echo "Error: $CONFIG_DIR directory does not exist. Exiting."
+    exit 1
+fi
+
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "Error: $CONFIG_FILE does not exist. Exiting."
+    exit 1
+fi
+
+# Read configuration from JSON config file
+project_directories=$(jq -r '.project_directories[]' "$CONFIG_FILE")
+flavors=$(jq -r '.flavors[]' "$CONFIG_FILE")
+declare -A google_play_console_urls
+while IFS="=" read -r key value; do
+    google_play_console_urls[$key]=$value
+done < <(jq -r '.google_play_console_urls | to_entries[] | .key + "=" + .value' "$CONFIG_FILE")
+browser_path=$(jq -r '.browser_path' "$CONFIG_FILE")
+destination_dir=$(jq -r '.destination_dir' "$CONFIG_FILE")
 
 # Loop through each directory and prompt the user to select
-selected_directory=$(for directory in "${project_directories[@]}"; do
+selected_directory=$(for directory in $project_directories; do
     # Log the Git branch name of the current directory
     git_branch=$(cd "$directory" && git rev-parse --abbrev-ref HEAD)
     echo "$directory - Git branch: $git_branch"
@@ -21,32 +38,21 @@ cd "$selected_directory"
 git_branch=$(git rev-parse --abbrev-ref HEAD)
 echo "Git branch of selected directory: $git_branch"
 
-# Prompt the user to select project code
-# PROJECT_CODE=$(echo -e "BUSMAP_TEST\nBUSMAP_MONTHLY_TICKET\nBUSMAP_RANKING\nMOTEL_MAP\nEMBUS\nEMBUS_DRIVER\nBUSMAP_HN\nBUSMAP_ADS_HUB\nSTUDENT_HUB\nWALLET_STAFF\nSTORE_VOUCHER\nPHENIKAA_CONNECT\nMAAS_CONNECT\nVINBUS\nHANH_TRINH_SO_DN" | fzf --prompt="Select project code: ")
-
 # Prompt the user to select build flavor
-selected_flavor=$(echo -e "Dev\nPrd" | fzf --prompt="Select build flavor: ")
+selected_flavor=$(echo "$flavors" | tr ' ' '\n' | fzf --prompt="Select build flavor: ")
+
+# Select the Google Play Console URL based on the flavor
+google_play_console_url="${google_play_console_urls[$selected_flavor]}"
 
 # Prompt the user to select clean or not clean build
 selected_option=$(echo -e "not clean\nclean" | fzf --prompt="Select build option: ")
-
-# Prompt the user to enter the selected version name
-# read -p "Enter selected version name: " selected_version_name
-
-# Check if the user entered a version name
-# if [ -z "$selected_version_name" ]; then
-#     echo "No version name entered. Exiting."
-#     exit 1
-# fi
-
-# Prompt the user to enter the description
-# read -p "Enter description (optional): " DESCRIPTION
 
 # Echo summary of user input in blue color
 echo -e "\033[34m"
 echo "Build Flavor: $selected_flavor"
 echo "Build Option: $selected_option"
-echo "Current Git Branch: $git_branch"
+echo "Git Branch: $git_branch"
+echo "Google Play Console URL: $google_play_console_url"
 echo -e "\033[0m"
 
 git pull
@@ -77,17 +83,12 @@ if [ -n "$selected_option" ] && [ -n "$selected_flavor" ]; then
         echo "chmod result: $chmod_result"
     fi
 
-
     # Run the selected build command
     $build_command
 
     # Check if the build was successful
     if [ $? -eq 0 ]; then
         # Green color for success message
-        # echo -e "\033[32mBuild successful. Uploading APK...\033[0m"
-        
-        # Set variables for curl request
-        # busMap/build/outputs/bundle/devRelease/busMap-dev-release.aab
         FILE_PATH="busMap/build/outputs/bundle/${selected_flavor}Release/busMap-${selected_flavor}-release.aab"
 
         # Get the APK file size
@@ -96,14 +97,10 @@ if [ -n "$selected_option" ] && [ -n "$selected_flavor" ]; then
         # Extract the file name from the file path
         file_name=$(basename "${FILE_PATH}")
 
-        echo -e "\033[32mBuild successful: $file_name ($file_size)"
+        echo -e "\033[32mBuild successful: $file_name ($file_size)\033[0m"
 
-        # complete that for me, that here i want to copy FILE_PATH to the destination folder: /Users/rumnguyen/Desktop/busmap-bump-app, but that file must be append system time in the end to unique
         # Get current timestamp
         timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
-
-        # Define destination directory
-        destination_dir="/Users/rumnguyen/Desktop/busmap-bump-app"
 
         # Define destination file name with timestamp
         destination_file="${destination_dir}/busMap-${selected_flavor}-release-${timestamp}.aab"
@@ -115,15 +112,12 @@ if [ -n "$selected_option" ] && [ -n "$selected_flavor" ]; then
         if [ $? -eq 0 ]; then
             echo "File copied successfully to: ${destination_file}"
             open ${destination_dir}
-            # Define Google Play Console URL
-            google_play_console_url="https://play.google.com/console/u/0/developers/9090446698342287343/app/4974327694095681934/tracks/production"
-            # Open Google Play Console URL in Chrome
-            open -a "Google Chrome" "$google_play_console_url"
 
+            # Open Google Play Console URL in Chrome
+            open -a "$browser_path" "$google_play_console_url"
         else
             echo "Error copying file."
         fi
-
     else
         # Red color for failure message
         echo -e "\033[31mBuild failed.\033[0m"
@@ -133,4 +127,3 @@ else
     echo "No option selected. Exiting."
     exit 1
 fi
-
